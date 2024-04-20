0314496a25503e27da8299242f0e444feb531590
kajiri <kajirikajiri@gmail.com>
2024-04-20 15:55:15 +0900

    Migrate

--- projects/rails-db-migrate-past-dates/db/schema.rb
+++ projects/rails-db-migrate-past-dates/db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_20_064943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "description"
   end
 
 end
