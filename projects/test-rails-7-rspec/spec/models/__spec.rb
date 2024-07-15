944cb75068e0a1c45086947bcbdc246346a8b502
kajiri <kajirikajiri@gmail.com>
2024-07-15 18:59:22 +0900

    Rails enum conflict test add

--- projects/test-rails-7-rspec/spec/models/__spec.rb
+++ projects/test-rails-7-rspec/spec/models/__spec.rb
@@ -10,4 +10,31 @@ describe type: :model do
       it { expect(Time.now).not_to eq Time.zone.now }
     end
   end
+  describe 'enum conflict' do
+    before(:all) do
+      m = ActiveRecord::Migration.new
+      m.verbose = false
+      m.create_table :samples do |t|
+        t.integer :status, null: false
+      end
+    end
+
+    after(:all) do
+      m = ActiveRecord::Migration.new
+      m.verbose = false
+      m.drop_table :samples
+    end
+
+    let(:klass) do
+      Class.new(ApplicationRecord) do |klass|
+        enum :status_one, { active: 0, inactive: 1 }, default: :active
+        enum :status_two, { active: 0, inactive: 1 }, default: :active
+        klass.table_name = 'samples'
+      end
+    end
+
+    it do
+      expect { klass }.to raise_error(ArgumentError, 'You tried to define an enum named "status_two" on the model "", but this will generate a instance method "active?", which is already defined by another enum.')
+    end
+  end
 end
