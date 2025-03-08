22d3c85bcebbda243be51e1404139a54f6de3fa0
kajirikajiri <kajirikajiri@gmail.com>
2025-03-09 00:47:40 +0900

    mpsを使う

--- projects/whisper/app.py
+++ projects/whisper/app.py
@@ -1,12 +1,16 @@
+# https://github.com/openai/whisper/pull/382#issuecomment-2484044391
+
 from flask import Flask, request, jsonify
-import whisper
+# import whisper
 import os
+import torch  # Add torch import
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # JSON のエンコード時に ASCII エスケープを無効化
 
-DEVICE = "cpu"
-model = whisper.load_model("base", device=DEVICE)
+#device = "cuda:0" if torch.cuda.is_available() else "cpu"
+device = "mps" if torch.backends.mps.is_available() else "cpu" 
 
 @app.route('/health')
 def health_check():
@@ -14,6 +18,7 @@ def health_check():
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe_audio():
+    print('transcribe_audio')
     if 'file' not in request.files:
         return jsonify({"error": "No file part"}), 400
     file = request.files['file']
@@ -22,7 +27,26 @@ def transcribe_audio():
     if file:
         file_path = os.path.join("/tmp", file.filename)
         file.save(file_path)
-        result = model.transcribe(file_path)
+        torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    
+        model_id = "openai/whisper-large-v3-turbo"
+        
+        model = AutoModelForSpeechSeq2Seq.from_pretrained(
+            model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+        )
+        model.to(device)
+        
+        processor = AutoProcessor.from_pretrained(model_id)
+        
+        pipe = pipeline(
+            "automatic-speech-recognition",
+            model=model,
+            tokenizer=processor.tokenizer,
+            feature_extractor=processor.feature_extractor,
+            torch_dtype=torch_dtype,
+            device=device,
+        )
+        result = pipe(file_path)
         os.remove(file_path)
         return jsonify({"transcription": result['text']}), 200
 
--- projects/whisper/readme.md
+++ projects/whisper/readme.md
@@ -4,24 +4,14 @@
 
 ## 前提条件
 
-- Docker
-- Docker Compose
-
-## セットアップ
-
-1. リポジトリをクローンします:
-    ```sh
-    git clone https://github.com/kajirikajiri/toy.git
-    cd toy/whisper
-    ```
-
-2. Dockerコンテナをビルドして起動します:
-    ```sh
-    docker-compose up --build
-    ```
-
-3. Flaskサーバーは `http://localhost:4673` で実行されます。
-
+1. ffmpegがインストールされていること
+2. python3 -m venv venv
+3. source venv/bin/activate
+4. pip install -r requirements.txt
+5. python3 app.py
+6. Flaskサーバーは `http://localhost:4673` で実行されます。
+7. curl -X POST http://localhost:4673/transcribe -F "file=@/Users/User/Downloads/test.m4a"
+8. Macのショートカットの場合は、オーディオを録音と内容を取得を使う。POSTリクエストで本文を要求「フォーム」キーfile、種類はファイル、値は録音したオーディオファイルを選択する。
 ## ヘルスチェック
 
 サーバーのヘルスをチェックするには、次のエンドポイントにアクセスします:
