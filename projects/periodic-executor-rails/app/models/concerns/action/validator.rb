e12c9eb1fbda72bff92e5b73eeadbc1263d9ba76
kajirikajiri <kajirikajiri@gmail.com>
2025-03-12 23:53:06 +0900

    fix-json-schema

--- projects/periodic-executor-rails/app/models/concerns/action/validator.rb
+++ projects/periodic-executor-rails/app/models/concerns/action/validator.rb
@@ -1,8 +1,61 @@
 module Action::Validator
   extend ActiveSupport::Concern
 
+  STEPS_SCHEMA = {
+    type: 'array',
+    items: {
+      oneOf: [
+        {
+          type: 'object',
+          properties: {
+            id: { type: 'string' },
+            name: { type: 'string', const: 'getCount' },
+            selector: { type: 'string' },
+          },
+        },
+        {
+          type: 'object',
+          properties: {
+            id: { type: 'string' },
+            name: { type: 'string', const: 'getInnerText' },
+            selector: { type: 'string' },
+          },
+        },
+        {
+          type: 'object',
+          properties: {
+            id: { type: 'string' },
+            name: { type: 'string', const: 'createTab' },
+            args: {
+              type: 'object',
+              properties: {
+                url: { type: 'string' },
+                waitLoadedMs: { type: 'integer' } 
+              }
+            }
+          },
+        },
+        {
+          type: 'object',
+          properties: {
+            id: { type: 'string' },
+            name: { type: 'string', const: 'trimText' },
+            args: {
+              type: 'object',
+              properties: {
+                replaced: { type: 'string' }, # stepのIDを指定し値を取得
+                pattern: { type: 'string' }, # 正規表現
+                replacement: { type: 'string' }, # 置換後の文字列
+              }
+            }
+          },
+        }
+      ]
+    }
+  }.freeze
+
   included do
-    validates :steps, json: true
+    validates :steps, json: { schema: STEPS_SCHEMA }
     validates :name, presence: true
   end
 end
--- projects/periodic-executor-rails/app/validators/json_validator.rb
+++ projects/periodic-executor-rails/app/validators/json_validator.rb
@@ -1,7 +1,12 @@
 class JsonValidator < ApplicationValidator
   def validate_each(record, attribute, value)
     JSON.parse(value)
+    if options[:schema].present?
+      JSON::Validator.validate!(options[:schema], value, strict: true)
+    end
   rescue JSON::ParserError
     record.errors.add(attribute, 'must be a valid JSON')
+  rescue JSON::Schema::ValidationError => e
+    record.errors.add(attribute, options[:message] || e.message)
   end
 end
