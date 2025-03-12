c05b28f2769219f961ebfdd54d37a8cb0f919c92
kajirikajiri <kajirikajiri@gmail.com>
2025-03-12 22:24:56 +0900

    replace-old-name-to-new-name

--- projects/periodic-executor-rails/app/graphql/new_episode_notifier_schema.rb
+++ projects/periodic-executor-rails/app/graphql/new_episode_notifier_schema.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class NewEpisodeNotifierSchema < GraphQL::Schema
+class PeriodicExecutorSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
--- projects/periodic-executor-rails/config/application.rb
+++ projects/periodic-executor-rails/config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module NewEpisodeNotifier
+module PeriodicExecutor
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.query_log_tags_enabled = true
