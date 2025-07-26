44b3c15655afef5c5be5e81057d2120e86d183b1
kajirikajiri <kajirikajiri@gmail.com>
2025-07-27 00:00:37 +0900

    Procオブジェクトに関するコメントを修正・追加
    - Procオブジェクトの説明を明確にするためにコメントを修正
    - Procとlambdaの動作に関する新しい例を追加
    - yieldの動作に関する説明を強化
    - Array#eachの類似品を実装し、使用例を追加

--- projects/test-ruby/tapとthen/proc.rb
+++ projects/test-ruby/tapとthen/proc.rb
@@ -1,5 +1,5 @@
-# procはcallできる。
-# ダイナミックローカル変数はprocのローカル変数として扱える。
+# Procオブジェクトはcallできる。
+# ダイナミックローカル変数はProcローカルの変数として扱える。
 var = 1
 f = Proc.new { var }
 var = 2
@@ -13,28 +13,62 @@ f = Proc.new do
 end
 p f.call
 
+# Proc.newでreturnすると、手続きオブジェクトを囲むメソッドを抜ける
+def method
+  f = Proc.new { return :proc }
+  f.call
+  return :method
+end
+p method
+
+# lambdaでは手続きオブジェクト自身を抜ける
+def method
+  f = lambda { return :lambda }
+  f.call
+  return :method
+end
+p method
+
+# lambdaのショートハンドは->
+def method
+  (-> { return :lambda }).call
+end
+p method
+
+# &:はSymbol.to_proc。lambdaを返す
+p :to_i.to_proc.lambda?
+
+# メソッドに&シンボルを渡すと、Symbol.to_procが呼ばれ、Procオブジェクトになり、ブロックが渡される
+p [1,2].each(&:to_s)
+
 # Kernel.procはProc.newと一緒
 f = proc { 5 }
 p f.call
 
-# blockを渡すならyieldが使える
-# procに&をつけてblockにできる
+# blockを渡さずにyieldを呼ぶと、LocalJumpErrorが発生する
+def method
+  yield
+end
+method rescue p $!
+
 # yieldはblockつきメソッドに渡されたblockを評価する
 def method # block付きメソッド
   yield if block_given? # blockが渡されてたら使うようにもできる
 end
 f = proc { 6 }
+# Procオブジェクトをブロックにして渡す
 puts method(&f)
+# ブロックを渡す
 puts method { 7 }
 
-# blockをprocに変換できる。
-def method(&f) # &fを定義だけすることもできる
+# ブロックをProcオブジェクトに変換できる。
+def method(&f) # &fを定義するだけもできる
   yield
 end
 f = proc { 8 }
 puts method(&f)
 
-def method(&f) # &をつけて、blockをprocとして受けとり、callすることもできる。
+def method(&f) # &をつけて、ブロックをProcオブジェクトとして受けとり、callすることもできる。
   f.call
 end
 f = proc { 9 }
@@ -44,3 +78,23 @@ puts method(&f)
 f = lambda { 10 } # 引数なしのlambda
 f.call(11) rescue p $! #<ArgumentError: wrong number of arguments (given 1, expected 0)>
 
+# ブロック付きメソッド呼び出しで、yieldが複数あればその分呼ばれる
+def method
+  yield 1
+  yield 2
+  yield 3
+end
+method { p it }
+
+# Array#eachの類似品
+def iich(arr)
+  idx = 0
+  while idx < arr.size
+    yield arr[idx]
+    idx += 1
+  end
+end
+
+iich(['a', 'b', 'c']) do
+  p it
+end
